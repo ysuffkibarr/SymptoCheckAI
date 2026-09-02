9c46f1d529855e61cd548ef7268ac96579e1a1d2
Yusuf Kibar <ysufkibar96@gmail.com>
2026-09-02 13:01:42 +0300

    feat:Persistent WAF Memory

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy.future import select
 from app.models import domain, schemas
+from app.models.domain import BlockedIP
 
 async def create_epidemic_log_async(db: AsyncSession, log: schemas.EpidemicLogCreate):
     db_log = domain.EpidemicLog(
@@ -18,3 +19,10 @@ async def get_recent_epidemic_logs_async(db: AsyncSession, limit: int = 1000):
     query = select(domain.EpidemicLog).order_by(domain.EpidemicLog.created_at.desc()).limit(limit)
     result = await db.execute(query)
     return result.scalars().all()
+
+async def blocked_ip_async(db: AsyncSession, ip_address: str, reason: str = None):
+    is_blocked = await is_ip_blocked_async(db, ip_address)
+    if is_blocked:
+        return
+
+    new_block = BlockedIP(ip_address=ip_address, reason=reason)
